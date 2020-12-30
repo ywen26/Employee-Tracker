d6842a113f5bad1782f9a8c47edca94328444ae3
Wednesday <yensonyu@gmail.com>
2020-12-29 23:09:10 -0800

    update schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY id
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,4),
+    department_id INT,
+    PRIMARY KEY id,
+    FOREIGN KEY(department_id) REFERENCES department(id) 
+);
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY id,
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(employee_id)
+);
