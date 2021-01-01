16b7c54272e78139356cd6749402362804f864e4
Wednesday <yensonyu@gmail.com>
2020-12-31 17:33:50 -0800

    create connection and search menu

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employee_DB;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
-    PRIMARY KEY id
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -14,7 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL(10,4),
     department_id INT,
-    PRIMARY KEY id,
+    PRIMARY KEY (id),
     FOREIGN KEY(department_id) REFERENCES department(id) 
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY id,
+    PRIMARY KEY (id),
     FOREIGN KEY(role_id) REFERENCES role(id),
     FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
--- index.js
+++ index.js
@@ -3,17 +3,72 @@ var mysql = require('mysql');
 var cTable = require('console.table');
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
   
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
   
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
   
-    // Your password
-    password: "password",
-    database: "employee_DB"
-  });
+  // Your password
+  password: "0226wind",
+  database: "employee_DB"
+});
 
-  
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+})
+
+function runSearch() {
+  inquirer.prompt({
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: [
+      "Add new departments",
+      "Add new roles",
+      "Add new employees",
+      "View the departments",
+      "View the roles",
+      "View the employees",
+      "Update employee roles",
+      "Exit"
+    ]
+  }).then(function(answer) {
+    switch(answer.action) {
+      case "Add new departments":
+        addDepartment();
+        break;
+      
+      case "Add new roles":
+        addRole();
+        break;
+
+      case "Add new employees":
+        addEmployee();
+        break;
+
+      case "View the departments":
+        viewDepartment();
+        break;
+
+      case "View the roles":
+        viewRole();
+        break;
+
+      case "View the employees":
+        viewEmployee();
+        break;
+      
+      case "Update employee roles":
+        updateRole();
+        break;
+
+      case "Exit":
+        connection.end();
+        break;
+    }
+  });
+}
