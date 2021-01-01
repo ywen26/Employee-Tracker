d18cecccdab2a7834d90064920dda2c5043c6f1a
Wednesday <yensonyu@gmail.com>
2020-12-31 16:06:37 -0800

    install npm packages

---
+++ index.js
@@ -0,0 +1,19 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "employee_DB"
+  });
+
+  
