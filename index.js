e10ceb4827671a34aeeb90a416aefd4f3fe1c7e3
Wednesday <yensonyu@gmail.com>
2021-01-01 18:54:27 -0800

    update function of adding

--- index.js
+++ index.js
@@ -72,3 +72,150 @@ function runSearch() {
     }
   });
 }
+
+function addDepartment() {
+  inquirer.prompt([
+    {
+      name: "department",
+      type: "input",
+      message: "Please enter the name of new department:"
+    }
+  ]).then(function(answer) {
+    connection.query("INSERT INTO department SET ?", 
+      {
+        name: answer.name
+      }, 
+      function(err, res) {
+        if (err) throw err;
+        // console.table(res);
+        runSearch();
+      }
+    );
+  });
+}
+
+function addRole() {
+  connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err;
+
+    var deptArr = res;
+    var allDept = [];
+    for (var i = 0; i < deptArr.length; i++) {
+      allDept.push(deptArr[i].name);
+    }
+
+    inquirer.prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Please enter the job title of new role:"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Please enter the salary of new role:"
+      },
+      {
+        name: "deptID",
+        type: "list",
+        message: "Please select the department of this role:",
+        choices: allDept
+      }
+    ]).then(function(answer) {
+      var departmentID;
+      for (var j = 0; j < res.length; j++) {
+        if (res[j].name === answer.deptID) {
+          departmentID = res[j].id;
+        }
+      }
+
+      connection.query("INSERT INTO role SET ?",
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: departmentID
+        },
+        function(err, res) {
+          if (err) throw (err);
+          // console.table(res);
+          runSearch();
+        }
+      );
+    });
+  });
+}
+
+function addEmployee() {
+  connection.query("SELECT * FROM role", function(err, res1) {
+    if (err) throw err;
+
+    var roleArr = res1;
+    var allRoles = [];
+    for (var i = 0; i < roleArr.length; i++) {
+      allRoles.push(roleArr[i].title);
+    }
+
+    connection.query("SELECT * FROM employee WHERE manager_id IS NULL", function(err, res2) {
+      if (err) throw err;
+
+      var managerArr = res2;
+      var allManagers = [];
+      for (var j = 0; j < managerArr.length; j++) {
+        allManagers.push(managerArr[j].last_name);
+      }
+
+      inquirer.prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "Please enter the first name of new employee:"
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "Please enter the last name of new employee:"
+        },
+        {
+          name: "roleTitle",
+          type: "list",
+          message: "Please select the title of new employee:",
+          choices: allRoles
+        },
+        {
+          name: "managerName",
+          type: "list",
+          message: "Please select the last name of the manager of new employee:",
+          choices: allManagers
+        }
+      ]).then(function(answer) {
+        var roleID;
+        for (var k = 0; k < res1.length; k++) {
+          if (res1[k].title === answer.roleTitle) {
+            roleID = res1[k].id;
+          }
+        }
+
+        var managerID;
+        for (var l = 0; l < res2.length; l++) {
+          if (res2[l].last_name === answer.managerName) {
+            managerID = res2[l].id;
+          }
+        }
+
+        connection.query("INSERT INTO employee SET ?",
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: roleID,
+            manager_id: managerID
+          },
+          function(err, res) {
+            if (err) throw (err);
+            // console.table(res);
+            runSearch();
+          }
+        );
+      });
+    });
+  });
+}
